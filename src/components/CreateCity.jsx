d86dba16eb8943f64e7278068c36b3164dd58d14
Marco Castellacci <marco.castellacci87@gmail.com>
2023-09-23 13:42:09 -0300

    Adding create form to user profile

---
+++ src/components/CreateCity.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import { useDispatch } from 'react-redux';
+
+export default function createCity() {
+    // const dispatch = useDispatch();
+
+
+    return (
+        <div>
+            <form action="" method="post">
+                <input type="text" id='' />
+                <label ></label>
+            </form>
+        </div>
+    )
+}
